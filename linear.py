bb4deb937fed661091fab28b8e2722dcfcdcb1bd
ngphongag <137142771+ngphongag@users.noreply.github.com>
2023-06-20 16:09:25 +0700

    Update linear.py

--- linear.py
+++ linear.py
@@ -3,27 +3,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
+# train_test_split chia dữ liệu từ data Input
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import streamlit as st
+#truyền dữ liệu input vào df
+df = pd.read_csv("nhom8.csv", encoding='latin-1')
 
-df = pd.read_csv("credit access.csv", encoding='latin-1')
+st.title("Nhóm 8 - Ứng dụng")
+st.write("## Xác định mức cho vay tối đa")
 
-st.title("Hồi quy tuyến tính")
-st.write("## Dự báo giá trị vay vốn của nông hộ")
-
-uploaded_file = st.file_uploader("Choose a file", type=['csv'])
+#upload file để huấn luyện mô hình
+uploaded_file = st.file_uploader("Chọn files dữ liệu:", type=['csv'])
+#nếu file có dữ liệu thì sẽ thực thi upload dữ liệu vào
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, encoding='latin-1')
     df.to_csv("data.csv", index = False)
 
+# X: giá trị input
 X = df.drop(columns=['giatri'])
+#y: giá trị target
 y = df['giatri']
 
+#test_size=0.2 chia dữ liệu 80 và 20;
+#random_state= 12
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state= 12)
 
+# model LinearRegression khởi tạo thuật toán LinearRegression
 model = LinearRegression()
 
+#
 model.fit(X_train, y_train)
 
 yhat_test = model.predict(X_test)
@@ -37,19 +46,33 @@ mse=mean_squared_error(y_test, yhat_test)
 rmse=mean_squared_error(y_test, yhat_test, squared=False)
 mae=mean_absolute_error(y_test, yhat_test)
 
-
-menu = ["Mục tiêu của mô hình", "Xây dựng mô hình", "Sử dụng mô hình để dự báo"]
-choice = st.sidebar.selectbox('Danh mục tính năng', menu)
+#xây dựng das
+menu = ["Mục tiêu của mô hình","Giới thiệu nhóm", "Xây dựng mô hình", "Sử dụng mô hình để dự báo"]
+choice = st.sidebar.selectbox('Danh mục Menu', menu)
 
 if choice == 'Mục tiêu của mô hình':    
     st.subheader("Mục tiêu của mô hình")
     st.write("""
-    ###### Mô hình được xây dựng để dự báo giá trị vay vốn của nông hộ dựa trên các biến đặc điểm chủ hộ, điều kiện của nông hộ.
+    ###### Mô hình được xây dựng để Xác định mức cho vay tối đa.
     """)  
     st.write("""###### Mô hình sử dụng thuật toán LinearRegression""")
     st.image("LSM.png")
     st.image("LSM_1.png")
 
+elif choice== "Giới thiệu nhóm":
+     st.subheader("Thành viên nhóm:")
+     st.write("##### 1. Nguyễn Đăng Viết Phong")
+     st.write("##### 2. Lê Duy Thịnh")
+     st.write("##### 3. Nguyễn Phú Hùng")
+     st.write("##### 4. Trần Quang Trung)
+     st.write("##### 5. Vũ Xuân Dũng")
+     st.write("##### 6. Nguyễn Minh Nhật Tùng")
+     st.write("##### 7. Nguyễn Mạnh Tú")
+     st.write("##### 8. Đỗ Quang Phát")
+     st.write("##### 9. Lê Thị Hằng")
+   # đưa hình nhóm vào
+     st.image("nhom8.png")
+               
 elif choice == 'Xây dựng mô hình':
     st.subheader("Xây dựng mô hình")
     st.write("##### 1. Hiển thị dữ liệu")
@@ -83,19 +106,17 @@ elif choice == 'Sử dụng mô hình để dự báo':
             st.dataframe(lines)
             # st.write(lines.columns)
             flag = True       
+    
+    # khai báo các biến nhập tay theo đúng tên các trường giá trị DATA
     if type=="Input":        
         git = st.number_input('Insert giatri')
-        DT = st.number_input('Insert DT')
         TN = st.number_input('Insert TN')
-        SPT = st.number_input('Insert SPT')
         GTC = st.number_input('Insert GTC')
         GD = st.number_input('Insert GD')
-        TCH = st.number_input('Insert TCH')
-        GT = st.number_input('Insert GT')
-        DV = st.number_input('Insert DV')
+       TCH = st.number_input('Insert TCH')
         VPCT = st.number_input('Insert VPCT')
         LS = st.number_input('Insert LS')
-        lines={'giatri':[git],'DT':[DT],'TN':[TN],'SPT':[SPT],'GTC':[GTC],'GD':[GD],'TCH':[TCH],'GT':[GT],'DV':[DV],'VPCT':[VPCT],'LS':[LS]}
+        lines={'giatri':[git],'TN':[TN],'GTC':[GTC],'GD':[GD],'TCH':[TCH],'VPCT':[VPCT],'LS':[LS]}
         lines=pd.DataFrame(lines)
         st.dataframe(lines)
         flag = True
